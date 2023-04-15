11ade96ce92696285fd6df10e130b8279b39ff13
Mike Gamaroff <mike@gamaroff.net>
2023-04-14 22:34:43 -0500

    Hashed password fix

--- routes/useAddUser.ts
+++ routes/useAddUser.ts
@@ -1,54 +1,46 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/authContext";
-import { UserContext } from "../context/userContext";
-import { User } from "../model/user";
-import { hashPassword } from "../util/hashPassword";
+import { useContext } from 'react';
+import { AuthContext } from '../context/authContext';
+import { UserContext } from '../context/userContext';
+import { User } from '../model/user';
 
 interface AddUserResponse {
-  success: boolean;
-  message: string;
+	success: boolean;
+	message: string;
 }
 
 export const useAddUser = () => {
-  const { users, setUsers } = useContext(UserContext);
-  const { login } = useContext(AuthContext);
+	const { users, setUsers } = useContext(UserContext);
+	const { login } = useContext(AuthContext);
 
-  const addUser = async (user: User): Promise<AddUserResponse> => {
-    try {
-      const hashedPassword = await hashPassword(user.password); // Hash the user's password
+	const addUser = async (user: User): Promise<AddUserResponse> => {
+		try {
+			const response = await fetch('/api/auth?action=register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(user) // Remove the password hashing from here
+			});
 
-      const response = await fetch("/api/auth?action=register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ...user,
-          password: hashedPassword, // Use the hashed password instead of the plain text password
-        }),
-      });
+			if (response.ok) {
+				const data = await response.json();
+				const userWithoutPassword = data.user ? { ...data.user, password: undefined } : null; // Unhash the password if the user exists
+				if (userWithoutPassword) {
+					setUsers([...users, userWithoutPassword]);
+				}
 
-      if (response.ok) {
-        const data = await response.json();
-        const userWithoutPassword = data.user
-          ? { ...data.user, password: undefined }
-          : null; // Unhash the password if the user exists
-        if (userWithoutPassword) {
-          setUsers([...users, userWithoutPassword]);
-        }
+				await login(user.email, user.password); // Authenticate the user using the hashed password
 
-        await login(user.email, user.password); // Authenticate the user using the hashed password
+				return { success: true, message: data.message };
+			} else {
+				const errorData = await response.json();
+				return { success: false, message: errorData.message };
+			}
+		} catch (error) {
+			console.error(error);
+			return { success: false, message: 'Internal server error' };
+		}
+	};
 
-        return { success: true, message: data.message };
-      } else {
-        const errorData = await response.json();
-        return { success: false, message: errorData.message };
-      }
-    } catch (error) {
-      console.error(error);
-      return { success: false, message: "Internal server error" };
-    }
-  };
-
-  return { addUser };
+	return { addUser };
 };

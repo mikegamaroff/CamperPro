5d79be6ddacf6214a289426e7489ae9d5ddee423
gabrielgamaroff <112270376+gabrielgamaroff@users.noreply.github.com>
2023-05-22 13:21:54 -0500

    Created component complete

---
+++ components/CampsiteImages.tsx
@@ -0,0 +1,60 @@
+import { FC, useEffect, useState } from 'react';
+import { Swiper as SwiperCore } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Campsite } from '../model/campsite';
+import { DotIndicator } from './DotIndicator';
+// eslint-disable-next-line css-modules/no-unused-class
+import sharedStyles from '../styles/shared.module.css';
+
+interface CampsiteImageProps {
+	campsite?: Campsite;
+}
+
+export const CampsiteImages: FC<CampsiteImageProps> = ({ campsite }) => {
+	const [swiperInstance, setSwiperInstance] = useState<SwiperCore | null>(null);
+	const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+	const images = campsite?.images;
+	const handleSlideChange = (swiper: SwiperCore) => {
+		const index = swiper.activeIndex;
+		setCurrentImageIndex(index);
+	};
+	const handleClick = (index: number) => {
+		swiperInstance?.slideTo(index);
+	};
+	useEffect(() => {
+		if (swiperInstance) {
+			swiperInstance.update();
+		}
+	}, [campsite, swiperInstance]);
+
+	return (
+		<Swiper
+			spaceBetween={10}
+			slidesPerView={1}
+			onSlideChange={handleSlideChange}
+			onSwiper={setSwiperInstance}
+			className={sharedStyles.imageContainer}
+		>
+			{images && images.length > 1 && (
+				<div className={sharedStyles.imageTracker}>
+					{images.map((image, index) => (
+						<div key={index} onClick={() => handleClick(index)} className={sharedStyles.imageTrackerDot}>
+							<DotIndicator on={currentImageIndex === index} />
+						</div>
+					))}
+				</div>
+			)}
+			{images &&
+				images.slice(0, 6).map((image, index) => (
+					<SwiperSlide key={index}>
+						<div className={`${sharedStyles.campsiteImage}`}>
+							<img
+								src={`/api/images/${image.id}.${image.contentType.split('/')[1]}`}
+								alt={`${campsite?.title} - Slide ${index + 1}`}
+							/>
+						</div>
+					</SwiperSlide>
+				))}
+		</Swiper>
+	);
+};
